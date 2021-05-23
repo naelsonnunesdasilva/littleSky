60fe97d00cd485660e4789e6e87f484175d2c5aa
Naelson Nunes <naelson.silva@novaescola.org.br>
2021-05-23 20:36:04 -0300

    inicio de páginas

--- App.js
+++ App.js
@@ -1,112 +1,59 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Filo = createStackNavigator();
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this kk
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+function TelaHome({navigation}){
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Tela Home</Text>
+            <Text>CFB Cursos</Text>
+            <Button
+                title="Tela Canal"
+                onPress={() => navigation.navigate('Canal')}
+            />
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+    );
+}
+
+function TelaCanal({navigation}){
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Tela Canal</Text>
+            <Text>Tela 2</Text>
+            <Button
+                title="Home"
+                onPress={() => navigation.navigate('Home')}
+            />
+            <Button
+                title="Voltar"
+                onPress={() => navigation.goBack()}
+            />
+        </View>
+    );
+}
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Filo.Navigator >
+                <Filo.Screen
+                    name="Home"
+                    component={TelaHome}
+                    options={{title: 'Tela Inicial'}}
+                />
+                <Filo.Screen
+                    name="Canal"
+                    component={TelaCanal}
+                    options={{title: 'Tela canal'}}
+                />
+            </Filo.Navigator>
+        </NavigationContainer>
+    )
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+    
+})
---
+++ pages/home.jsx
@@ -0,0 +1,9 @@
+export default function Home({navigation}){
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            
+
+            
+        </View>
+    );
+}
